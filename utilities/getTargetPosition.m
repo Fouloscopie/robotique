3719f14182413e2dd0a7ac5526f4bd4fb7ff2fc1
Fouloscopie <82611914+Fouloscopie@users.noreply.github.com>
2021-04-26 00:03:41 +0200

    Update getTargetPosition.m

--- utilities/getTargetPosition.m
+++ utilities/getTargetPosition.m
@@ -6,7 +6,7 @@ while ok==0
     x_target = rand.*2.8 - 1.4 ;
     y_target = rand.*1.6 - 0.8 ;
     
-    if abs(x_target)>0.5 && abs(y_target)>0.4
+    if abs(x_target)>0.5 || abs(y_target)>0.4
         ok = 1 ;
     end
     
